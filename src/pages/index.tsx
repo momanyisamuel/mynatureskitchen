b238c87a43c0af0d074b91c46f24a2d427300356
momanyisamuel <momanyisamuel48@gmail.com>
2023-05-01 15:35:58 +0300

    mobile responsive homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
-import { type NextPageWithLayout } from './_app';
+import { type NextPageWithLayout } from "./_app";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Salad } from "lucide-react";
 import Link from "next/link";
-import { Input } from '@/components/ui/input';
+import { Input } from "@/components/ui/input";
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
       <main className="text-atlantis-900">
         <div className="items-center justify-center gap-2 pt-5 sm:flex sm:px-8">
-          <div className="mt-14 px-8 py-2 flex flex-col items-center gap-6 text-center sm:mt-0 sm:w-1/2 sm:px-5">
-            <p className='uppercase text-base leading-7'>My Natures Kitchen</p>
-            <h2 className="mb-2 px-8 text-4xl leading-[43px] font-medium font-serif">
+          <div className="mt-14 flex flex-col items-center gap-6 px-8 py-2 text-center sm:mt-0 sm:w-1/2 sm:px-5">
+            <p className="text-base uppercase leading-7">My Natures Kitchen</p>
+            <h2 className="mb-2 px-8 font-serif text-4xl font-medium leading-[43px]">
               The time to enjoy the Whole food plant based lifestyle is now.
             </h2>
             <p className="px-8 text-lg">
@@ -21,13 +21,18 @@ const Home: NextPageWithLayout = () => {
               everyone to enjoy a successful whole food plant-based eating
               lifestyle for a healthier you one step at a time.
             </p>
-            <Link href="/classes" className=''>
-              <div className="text-lg text-atlantis-50 bg-atlantis-300 w-[28rem] px-2 block py-3">
-              <p className='hover:underline'>Upcoming zoom class: Introduction to How Foods Fight Cancer</p>
+            <Link href="/classes" className="">
+              <div className="block w-[28rem] bg-atlantis-400 px-2 py-3 text-lg text-atlantis-900">
+                <p className="hover:underline">
+                  Upcoming zoom class: Introduction to How Foods Fight Cancer
+                </p>
               </div>
             </Link>
             <div className="">
-              <Button variant="outline" className="inline-flex text-lg border-atlantis-900">
+              <Button
+                variant="outline"
+                className="inline-flex border-atlantis-900 text-lg"
+              >
                 Sign up for our food for life classes
               </Button>
             </div>
@@ -59,61 +64,83 @@ const Home: NextPageWithLayout = () => {
           </div>
         </div>
 
-        <div className="mt-5 bg-atlantis-600 py-8 sm:mx-8">
-          <div className="flex items-center justify-center">
-            <div className="w-2/6"><h2 className='text-atlantis-100'>Join our  email list for a free one week meal plan, nutrition classes and other upcoming events!</h2></div>
-            <div className="w-3/6 flex gap-2 justify-end">
-            <Input placeholder='Your email address' className='w-4/6 text-atlantis-100' />
-            <Button
-              variant="outline"
-              className="bg-atlantis-100 border-atlantis-100 hover:bg-atlantis-200 w-2/6"
-            >
-              Subscribe to our newsletter
-            </Button>
+        <div className="mt-5 bg-atlantis-400 text-atlantis-900 py-8 sm:mx-8">
+          <div className=" w-full flex flex-col sm:flex-row items-center justify-center px-8">
+            <div className="w-full sm:w-3/6">
+              <h2 className="">
+                Join our email list for a free one week meal plan, nutrition
+                classes and other upcoming events!
+              </h2>
+            </div>
+            <div className="flex flex-col sm:flex-row w-full sm:w-3/6 justify-center items-center sm:justify-end gap-2">
+              <Input
+                placeholder="Your email address"
+                className="w-[70%]"
+              />
+              <Button
+                variant="outline"
+                className="w-[30%] border-atlantis-100 bg-atlantis-100 hover:bg-atlantis-200"
+              >
+                Subscribe to our newsletter
+              </Button>
             </div>
           </div>
         </div>
 
         <div className="px-8 py-5">
-          <div className="mb-8 leading-8">
-            <p className="mb-2 pt-2 text-sm font-semibold uppercase">
-              Our services
+          <div className="mb-8 px-8">
+            <p className="mb-2 px-8 text-center font-serif text-4xl font-medium leading-[43px]">
+              What we offer
             </p>
-            <h2 className="font-base scroll-m-20 text-4xl leading-10 tracking-tight lg:text-5xl">
-              At My Natures Kitchen LLC
-            </h2>
-            <p className="mt-4">
-              we are passionate about helping people embrace the Whole Food
-              Plant Based lifestyle. Our services are designed to make it easy
-              and convenient for you to enjoy delicious and nutritious
-              plant-based meals every day. Here are some of the ways we can
-              support you:
+            <p className="mt-4 text-center text-lg">
+              At My Natures Kitchen LLC we are passionate about helping people
+              embrace the Whole Food Plant Based lifestyle. Our services are
+              designed to make it easy and convenient for you to enjoy delicious
+              and nutritious plant-based meals every day. Here are some of the
+              ways we can support you
             </p>
           </div>
-
-          <div className="grid w-full grid-cols-3 gap-4">
-            <div className="relative mb-4 border border-t-8 border-atlantis-500 bg-white px-6 py-4 shadow-lg">
-              <span className="absolute top-[-28px] inline-flex h-12 w-12 items-center justify-center rounded-full bg-atlantis-500">
-                <Salad className="h-8 w-8 text-white" />
-              </span>
-              <p className="mb-2 pt-2 text-sm font-semibold uppercase">
-                Food for Life Classes
-              </p>
-              <p>
+          <div className="bg-atlantis-400 px-8 flex flex-col sm:flex-row justify-between items-center py-11 gap-4 mb-8 border">
+            <div className="relative mb-4 w-full sm:w-3/6 border">
+              <div className="mb-8 flex justify-between border-b border-atlantis-900">
+                <p className="mb-2 pt-2 font-serif text-xl font-medium">
+                  Food for Life Classes
+                </p>
+                <span className="">
+                  <Salad className="h-8 w-8 text-atlantis-900" />
+                </span>
+              </div>
+              <p className="text-lg">
                 Our classes are designed to teach you the basics of plant-based
                 cooking and nutrition. Whether you're a beginner or an
                 experienced cook, we have classes that will help you develop new
                 skills and explore new flavors.
               </p>
             </div>
-            <div className="relative mb-4 border border-t-8 border-atlantis-500 bg-white px-6 py-4 shadow-lg">
-              <span className="absolute top-[-28px] inline-flex h-12 w-12 items-center justify-center rounded-full bg-atlantis-500">
-                <Salad className="h-8 w-8 text-white" />
-              </span>
-              <p className="mb-2 pt-2 text-sm font-semibold uppercase">
-                Meal Plans
-              </p>
-              <p>
+            <div className="w-3/6 border flex flex-col sm:flex-row justify-center items-center">
+            <div className="w-full sm:w-[50%] flex justify-center items-center border">
+              <Image src="/assets/FFL Licensed Instructor Logo.png" width={183} height={212} alt="food license"/>
+            </div>
+            <div className="w-full sm:w-[50%] flex  justify-center items-center border">
+              <div className="w-full p-5 bg-atlantis-600 flex flex-col justify-center items-center">
+              <p className="text-lg text-atlantis-50 mb-2">Upcoming zoom class:Introduction to How Foods Fight Cancer</p>
+              <Button variant="secondary" className="w-full">Learn more</Button>
+              </div>
+            </div>
+            </div>
+          </div>
+
+          <div className="grid w-full grid-cols-1 sm:grid-cols-2 gap-8">
+            <div className="relative mb-4 px-11">
+              <div className="mb-8 flex justify-between border-b border-atlantis-900">
+                <p className="mb-2 pt-2 font-serif text-xl font-medium">
+                  Meal Plans
+                </p>
+                <span className="">
+                  <Salad className="h-8 w-8 text-atlantis-900" />
+                </span>
+              </div>
+              <p className="text-lg">
                 We offer customizable meal plans that are tailored to your
                 preferences and dietary needs. Our plans include a variety of
                 delicious plant-based meals and snacks that are designed to keep
@@ -121,68 +148,78 @@ const Home: NextPageWithLayout = () => {
               </p>
             </div>
 
-            <div className="relative mb-4 border border-t-8 border-atlantis-500 bg-white px-6 py-4 shadow-lg">
-              <span className="absolute top-[-28px] inline-flex h-12 w-12 items-center justify-center rounded-full bg-atlantis-500">
-                <Salad className="h-8 w-8 text-white" />
-              </span>
-              <p className="mb-2 pt-2 text-sm font-semibold uppercase">
-                Meal Prep
-              </p>
-              <p>
+            <div className="relative mb-4 px-11">
+              <div className="mb-8 flex justify-between border-b border-atlantis-900">
+                <p className="mb-2 pt-2 font-serif text-xl font-medium">
+                  Meal Prep
+                </p>
+                <span className="">
+                  <Salad className="h-8 w-8 text-atlantis-900" />
+                </span>
+              </div>
+              <p className="text-lg">
                 We offer meal prep services at specific locations, making it
                 easy for you to enjoy healthy and delicious meals even when
                 you're short on time.
               </p>
             </div>
-            <div className="relative mb-4 border border-t-8 border-atlantis-500 bg-white px-6 py-4 shadow-lg">
-              <span className="absolute top-[-28px] inline-flex h-12 w-12 items-center justify-center rounded-full bg-atlantis-500">
-                <Salad className="h-8 w-8 text-white" />
-              </span>
-              <p className="mb-2 pt-2 text-sm font-semibold uppercase">
-                Baked goods
-              </p>
-              <p>
+            <div className="relative mb-4 px-11">
+              <div className="mb-8 flex justify-between border-b border-atlantis-900">
+                <p className="mb-2 pt-2 font-serif text-xl font-medium">
+                  Baked goods
+                </p>
+                <span className="">
+                  <Salad className="h-8 w-8 text-atlantis-900" />
+                </span>
+              </div>
+              <p className="text-lg">
                 We offer a range of baked goods that are made with high-quality
                 plant-based ingredients. Our products are available for sale at
                 specific locations and are perfect for a quick snack or a
                 special treat.
               </p>
             </div>
-            <div className="relative mb-4 border border-t-8 border-atlantis-500 bg-white px-6 py-4 shadow-lg">
-              <span className="absolute top-[-28px] inline-flex h-12 w-12 items-center justify-center rounded-full bg-atlantis-500">
-                <Salad className="h-8 w-8 text-white" />
-              </span>
-              <p className="mb-2 pt-2 text-sm font-semibold uppercase">
-                Easy to follow recipes
-              </p>
-              <p>
+            <div className="relative mb-4 px-11 ">
+              <div className="mb-8 flex justify-between border-b border-atlantis-900">
+                <p className="mb-2 pt-2 font-serif text-xl font-medium">
+                  Easy to follow recipes
+                </p>
+                <span className="">
+                  <Salad className="h-8 w-8 text-atlantis-900" />
+                </span>
+              </div>
+              <p className="text-lg">
                 We provide you with easy-to-follow plant-based recipes that are
                 designed to fit your taste and budget. Our recipes are perfect
                 for busy weeknights or special occasions.
               </p>
             </div>
-            <div className="relative mb-4 border border-t-8 border-atlantis-500 bg-white px-6 py-4 shadow-lg">
-              <span className="absolute top-[-28px] inline-flex h-12 w-12 items-center justify-center rounded-full bg-atlantis-500">
-                <Salad className="h-8 w-8 text-white" />
-              </span>
-              <p className="mb-2 pt-2 text-sm font-semibold uppercase">
-                Healthiest Cookware
-              </p>
-              <p>
+            <div className="relative mb-4 px-11 ">
+              <div className="mb-8 flex justify-between border-b border-atlantis-900">
+                <p className="mb-2 pt-2 font-serif text-xl font-medium">
+                  Healthiest Cookware
+                </p>
+                <span className="">
+                  <Salad className="h-8 w-8 text-atlantis-900" />
+                </span>
+              </div>
+              <p className="text-lg">
                 We teach you about the healthiest cookware in the world. Our
                 experts will help you choose the best cookware for your needs
                 and show you how to use it to create delicious and nutritious
                 plant-based meals.
               </p>
             </div>
-            <div className="relative mb-4 border border-t-8 border-atlantis-500 bg-white px-6 py-4 shadow-lg">
-              <span className="absolute top-[-28px] inline-flex h-12 w-12 items-center justify-center rounded-full bg-atlantis-500">
-                <Salad className="h-8 w-8 text-white" />
-              </span>
-              <p className="mb-2 pt-2 text-sm font-semibold uppercase">
-                Lunch and Learn
-              </p>
-              <p>
+            <div className="relative mb-4 px-11">
+              <div className="mb-8 flex justify-between border-b border-atlantis-900">
+                <p className="mb-2 pt-2 font-serif text-xl font-medium">
+                  Lunch and Learn
+                </p>
+                <span className="">
+                  <Salad className="h-8 w-8 text-atlantis-900" />
+                </span>
+              </div>
+              <p className="text-lg">
                 We host lunch and learn sessions that are designed to help you
                 learn more about plant-based nutrition and cooking. These
                 sessions are a great way to meet other people who are passionate
@@ -192,18 +229,11 @@ const Home: NextPageWithLayout = () => {
             </div>
           </div>
         </div>
-        <div className="flex gap-4 bg-atlantis-300 px-8 py-11">
-          <div className="w-2/6">
-            <p>What Our Customers Say</p>
-            <p>
-              We can tell you about the amazing things we can do for your
-              business, but we think our customers can tell you better. After
-              all, what they think is what really matters.
-            </p>
-          </div>
-          <div className="w-4/6">
-            <div className=" w-[25rem] bg-slate-50 p-4 shadow-lg">
-              <p className="font-base scroll-m-20 text-2xl tracking-tight">
+        <div className="mx-8 flex gap-4 bg-atlantis-400 px-8 py-11 text-center text-atlantis-50">
+          <div className="">
+            <div className="">
+              <p>What Our Customers Say</p>
+              <p className="scroll-m-20 font-serif text-3xl font-medium tracking-tight">
                 My Natures Kitchen will be at our final market of the season,
                 Sept. 18, 12 pm - 3 pm at Cowles Commons. Leah, the owner, is
                 excited to share her love for plant-based cooking and baking
